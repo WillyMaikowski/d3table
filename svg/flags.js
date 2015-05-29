ee87139f10c0b3e144f8f02e3682d939c3263558
Willy Maikowski <willy.maikowski@gmail.com>
2015-05-29 04:51:26 -0300

    animations

--- svg/flags.js
+++ svg/flags.js
@@ -1,3 +1,18 @@
+var conf = {
+  Argentina:  { c: '#FBAA19', f: 'flag-ARG', g: 'B' },
+  Bolivia:    { c: '#FFD400', f: 'flag-BOL', g: 'A' },
+  Brazil:     { c: '#FFF200', f: 'flag-BRA', g: 'C' },
+  Chile:      { c: '#ED1C24', f: 'flag-CHI', g: 'A' },
+  Colombia:   { c: '#FFCB05', f: 'flag-COL', g: 'C' },
+  Ecuador:    { c: '#FFCB05', f: 'flag-ECU', g: 'A' },
+  Jamaica:    { c: '#FFD400', f: 'flag-JAM', g: 'B' },
+  Mexico:     { c: '#ED1C24', f: 'flag-MEX', g: 'A' },
+  Paraguay:   { c: '#ED1C24', f: 'flag-PAR', g: 'B' },
+  Peru:       { c: '#ED1C24', f: 'flag-PER', g: 'C' },
+  Uruguay:    { c: '#FBAA19', f: 'flag-URU', g: 'B' },
+  Venezuela:  { c: '#FFD400', f: 'flag-VEN', g: 'C' }
+}
+
 var fstar = function( x, y, w ) {
       var sz = 3*w/2, yy = y+w/2;
       return [
@@ -24,48 +39,26 @@ var createFlag = function( svgContainer, xCoord, yCoord, flagName, winProba, loo
   var p = { x: 90, y: 35 + yCoord };
   var w = 10;
   var l = 300;
-  var arrow_ini = svgContainer.polygon( [p.x, p.y, p.x+l/3, p.y, p.x+l/3, p.y+w, p.x, p.y+w, p.x+(w/2),p.y+(w/2)] ).attr( { fill: color, class: flagName, visibility: 'hidden' } ); 
-  var arrow_pasar = svgContainer.polygon( [p.x+l/3, p.y, p.x+l, p.y, p.x+l+(w/2), p.y+(w/2), p.x+l, p.y+w, p.x+l/3, p.y+w] ).attr( { fill: color, class: flagName, visibility: 'hidden' } );
-  var arrow_nopasar = svgContainer.polygon( [p.x+l/3,p.y+w,p.x+l/3,p.y+w+l/6,p.x+l/3-w/2,p.y+l/6+3*w/2,p.x+l/3-w,p.y+w+l/6,p.x+l/3-w,p.y+w] ).attr( { fill: '#DEDEDE', class: flagName, visibility: 'hidden' } );
-  var star_pasar = svgContainer.polygon( fstar( p.x+l+10, p.y, w ) ).attr( { fill: color, class: flagName, visibility: 'hidden' } );
-  var pasar_txt = svgContainer.text( p.x+l+10.5, p.y+w/2+4, winProba ).attr( { fill: '#FFF', fontFamily: 'Bree Serif', class: flagName, visibility: 'hidden' } );
-  var star_nopasar = svgContainer.polygon( fstar( p.x+l/3-2*w, p.y+l/6+3*w, w ) ).attr( { fill: '#DEDEDE', class: flagName, visibility: 'hidden' } );
-  var nopasar_txt = svgContainer.text( p.x+l/3-2*w+5, p.y+l/6+7*w/2+4, looseProba ).attr( { fill: '#FFF', fontFamily: 'Bree Serif', class: flagName, visibility: 'hidden' } );
+  var arrow_ini = svgContainer.polygon( [p.x, p.y, p.x+l/3, p.y, p.x+l/3, p.y+w, p.x, p.y+w, p.x+(w/2),p.y+(w/2)] ).attr( { fill: color, class: flagName, opacity: 0 } ); 
+  var arrow_pasar = svgContainer.polygon( [p.x+l/3, p.y, p.x+l, p.y, p.x+l+(w/2), p.y+(w/2), p.x+l, p.y+w, p.x+l/3, p.y+w] ).attr( { fill: color, class: flagName, opacity: 0 } );
+  var arrow_nopasar = svgContainer.polygon( [p.x+l/3,p.y+w,p.x+l/3,p.y+w+l/6,p.x+l/3-w/2,p.y+l/6+3*w/2,p.x+l/3-w,p.y+w+l/6,p.x+l/3-w,p.y+w] ).attr( { fill: '#DEDEDE', class: flagName, opacity: 0 } );
+  var star_pasar = svgContainer.polygon( fstar( p.x+l+10, p.y, w ) ).attr( { fill: color, class: flagName, opacity: 0 } );
+  var pasar_txt = svgContainer.text( p.x+l+10.5, p.y+w/2+4, winProba ).attr( { fill: '#FFF', fontFamily: 'Bree Serif', class: flagName, opacity: 0 } );
+  var star_nopasar = svgContainer.polygon( fstar( p.x+l/3-2*w, p.y+l/6+3*w, w ) ).attr( { fill: '#DEDEDE', class: flagName, opacity: 0 } );
+  var nopasar_txt = svgContainer.text( p.x+l/3-2*w+5, p.y+l/6+7*w/2+4, looseProba ).attr( { fill: '#FFF', fontFamily: 'Bree Serif', class: flagName, opacity: 0 } );
   Snap.load( 'sprites.svg', function( f ) {
     var flag = f.select( '#'+flagName ).attr({ x:xCoord, y:yCoord, viewBox: '0 0 600 600' });
     flag.mouseover( function() {
-      svgContainer.selectAll( '.'+flagName ).animate( { visibility: 'visible' }, 500 );
+      $.each( conf, function( k, v ) {
+        if( v.f != flagName ) svgContainer.selectAll( '.'+v.f ).animate( { opacity: 0 }, 200 );
+      } );
+      svgContainer.selectAll( '.'+flagName ).animate( { opacity: 1 }, 500 );
     } );
     svgContainer.append( flag );
   } );
 
 }
 
-var createSvgContainer = function( svgId ){
-	var body = document.getElementsByTagName('body')[0];
-  var svg = document.createElement('svg');
-  svg.id = svgId;
-  
-  var br = document.createElement('br');
-   
-  body.appendChild(svg);
-  body.appendChild(br);
-}
-
-var conf = {
-  Argentina:  { c: '#FBAA19', f: 'flag-ARG', g: 'B' },
-  Bolivia:    { c: '#FFD400', f: 'flag-BOL', g: 'A' },
-  Brazil:     { c: '#FFF200', f: 'flag-BRA', g: 'C' },
-  Chile:      { c: '#ED1C24', f: 'flag-CHI', g: 'A' },
-  Colombia:   { c: '#FFCB05', f: 'flag-COL', g: 'C' },
-  Ecuador:    { c: '#FFCB05', f: 'flag-ECU', g: 'A' },
-  Jamaica:    { c: '#FFD400', f: 'flag-JAM', g: 'B' },
-  Mexico:     { c: '#ED1C24', f: 'flag-MEX', g: 'A' },
-  Paraguay:   { c: '#ED1C24', f: 'flag-PAR', g: 'B' },
-  Peru:       { c: '#ED1C24', f: 'flag-PER', g: 'C' },
-  Uruguay:    { c: '#FBAA19', f: 'flag-URU', g: 'B' },
-  Venezuela:  { c: '#FFD400', f: 'flag-VEN', g: 'C' }
-}
 
 var figHeight = 90;
 var figWidth = 480;
